a46a6e75add96a2810966469b56ff75049eb43af
yashmane987 <yashmane4045@gmail.com>
2025-09-05 16:13:27 +0530

    done

--- 2_interview.js
+++ 2_interview.js
@@ -1,24 +1,19 @@
-
 //Show addition of 10 but show that element in index in output
-const arr = [ 8,1,5,4, 6, 11]
-const ans = []
-for(let i=0;i<arr.length-1;i ++){
-    for(let j = i+1; j<arr.length;j++){
-        if(arr[i]+arr[j] == 10){
-            ans.push(i);
-            ans.push(j);
-        }
+const arr = [8, 1, 5, 4, 6, 11];
+const ans = [];
+for (let i = 0; i < arr.length - 1; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] + arr[j] == 10) {
+      ans.push(i);
+      ans.push(j);
     }
+  }
 }
-console.log(ans)
+console.log(ans);
 
 // in JavaScript checks if a value is Not-a-Number
 // it will return  a boolean value
-console.log(isNaN("Yash"))
-
-
-
-
+console.log(isNaN("Yash"));
 
 // show this string in alteranate way ydaesvhe
 let str1 = "developer";
@@ -38,35 +33,30 @@ console.log(result);
 
 // foreach - not create a new array
 //           in foreach method array are modified
-          
-// map -   create a new array 
-//         in map method array are not modified
 
+// map -   create a new array
+//         in map method array are not modified
 
 // ex foreach
 const numbers = [1, 2, 3, 4];
 let sum = 0;
 
-numbers.forEach(num => {
-  sum += num;  
+numbers.forEach((num) => {
+  sum += num;
 });
 
-console.log(sum); 
+console.log(sum);
 
 // ex map
 
 const numb = [1, 2, 3, 4];
 
-const doubled = numb.map(num => num * 2);
+const doubled = numb.map((num) => num * 2);
 
 console.log(doubled);
 
-
-
-
 // pure function:- Always produces the same output for the same input.
 //impure function :- Can produce different outputs for the same input.
-
 
 // ex pure function
 
@@ -74,9 +64,8 @@ function add(a, b) {
   return a + b;
 }
 
-console.log(add(2, 3)); 
-console.log(add(2, 3)); 
-
+console.log(add(2, 3));
+console.log(add(2, 3));
 
 //ex impure function
 
@@ -86,16 +75,15 @@ function multiply(num) {
   return num * factor;
 }
 
-console.log(multiply(5)); 
+console.log(multiply(5));
 factor = 3;
-console.log(multiply(5)); 
+console.log(multiply(5));
 
-
-//What is Annomus function 
+//What is Annomus function
 
 // anonymous function is a function without a name.
 
-const greet = function() {
+const greet = function () {
   console.log("Hello!");
 };
 
@@ -108,10 +96,9 @@ greet();
 
 // ex rest operter
 
-const array = [1,2,3,4,5];
-const d = [...array,6,7,8]
-console.log(d)
-
+const array = [1, 2, 3, 4, 5];
+const d = [...array, 6, 7, 8];
+console.log(d);
 
 // ex spread operater
 
@@ -120,19 +107,18 @@ function sum1(...numbers) {
   return numbers.reduce((acc, val) => acc + val, 0);
 }
 
-console.log(sum1(1, 2, 3, 4)); 
+console.log(sum1(1, 2, 3, 4));
 
 // differnce Primitive and non-Primitive
 
-// Primitive datatypes - Number,string,Boolean,undefined,Null are immutable 
+// Primitive datatypes - Number,string,Boolean,undefined,Null are immutable
 
 // non-Primitive - array,objects,functions are muttable
-
 
 // primative and non-primative when compare non-p matches the refernces.
 console.log({} == {});
 // console.log({} === {});
 
-// Which one is better async await , promises 
+// Which one is better async await , promises
 
-// better is asyn and await are latest version of promise 
+// better is asyn and await are latest version of promise
