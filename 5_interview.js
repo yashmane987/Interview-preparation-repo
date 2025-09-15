a51affe64332636f264b4aa4ea6cee4d80d7e387
yashmane987 <yashmane4045@gmail.com>
2025-09-15 19:54:51 +0530

    new one

---
+++ 5_interview.js
@@ -0,0 +1,25 @@
+// slice and splice
+// slice method are immutable and splice method is mutabale we can add , rmove and repalcing element
+
+let arr=[2,3,4,5,6];
+let result = arr.slice(1,3);//34
+console.log(result);
+
+
+let arr1 =[2,3,4,5,6];
+arr1.splice(1,2);//256
+console.log(arr1)
+
+
+// console.log([]===[])  it will check where it is store
+// console.log([]==[])
+
+
+
+// what is class attribute we can use for use multiple element same name for css 
+
+//require attribute use in input tag for this field must be write
+
+//i and em tag 
+
+// i is italic text and em is Emphasizes text with semantic
